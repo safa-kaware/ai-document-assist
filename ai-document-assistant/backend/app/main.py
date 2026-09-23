78c13b17215cc02bec246329ddf8a2ed2fc17cdb
safa-kaware <kawaresafa143@gmail.com>
2026-09-23 09:42:12 +0530

    allowed origins

--- ai-document-assistant/backend/app/main.py
+++ ai-document-assistant/backend/app/main.py
@@ -8,7 +8,10 @@ app = FastAPI(title="AI Document Assistant API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+       allow_origins=[
+        "http://localhost:5173",  # local dev
+        "https://ai-document-assist-eight.vercel.app",  # your deployed frontend
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
